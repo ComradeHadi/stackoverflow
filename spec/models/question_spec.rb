4afb467d5fc399373498d61f3d21ed0629969dd3
Анатолий Удальцов <cmdx@tender.pro>
2015-03-11 00:30:57 +0300

    rspec: destroy question should destroy dependent answers

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+
+  it { should have_many(:answers).dependent(:destroy) }
+
+  describe "destroy question" do
+    it "should destroy dependent answers" do
+      question1 = FactoryGirl.create(:question)
+      answer1 = FactoryGirl.create(:answer, question_id: question1.id)
+      answer2 = FactoryGirl.create(:answer, question_id: question1.id)
+
+      expect { question1.destroy }.to change{ Answer.count }.from(2).to(0)
+    end
+  end
 end
