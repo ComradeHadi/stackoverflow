753f0116f5ee4e25389c53930a9964fb2b14e6dc
Анатолий Удальцов <cmdx@tender.pro>
2015-03-11 12:12:46 +0300

    test questions#index

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe "GET #index" do
+    before do
+      @questions = FactoryGirl.create_list(:question, 2)
+      get :index
+    end
 
+    it 'populates questions array' do
+      expect(assigns(:questions)).to match_array(@questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
 end
