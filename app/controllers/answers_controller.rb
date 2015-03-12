58fdbc4cf37b1db25bd5d44f8664e993287bba88
Анатолий Удальцов <cmdx@tender.pro>
2015-03-12 11:19:34 +0300

    refactor controller Answers - before_action :load_question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :load_question, only: [:create, :update, :destroy]
   before_action :load_answer, only: [:show, :edit, :destroy]
 
   def index
@@ -16,7 +17,6 @@ class AnswersController < ApplicationController
   end
 
   def create
-    @question = Question.find(params[:question_id])
     @answer = @question.answers.new(answer_params)
     if @answer.save
       redirect_to question_answer_path(@question, @answer)
@@ -26,7 +26,6 @@ class AnswersController < ApplicationController
   end
 
   def update
-    @question = Question.find(params[:question_id])
     @answer = Answer.find(params[:id])
     if @answer.update(answer_params)
       redirect_to question_answer_path(@question, @answer)
@@ -36,12 +35,14 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-    @question = Question.find(params[:question_id])
     @answer.destroy
     redirect_to question_answers_path(@question)
   end
 
   private
+  def load_question
+    @question = Question.find(params[:question_id])
+  end
   def load_answer
     @answer = Answer.find(params[:id])
   end
