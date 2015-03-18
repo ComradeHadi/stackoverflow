bd2bb97b48b615b73f6910b9f64aa5d4037b78dc
Анатолий Удальцов <cmdx@tender.pro>
2015-03-18 12:12:42 +0300

    test model User: dependent restrict_with_exception

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many :answers }
-  it { should have_many :questions }
+  it { should have_many(:answers).dependent(:restrict_with_exception) }
+  it { should have_many(:questions).dependent(:restrict_with_exception) }
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password}
