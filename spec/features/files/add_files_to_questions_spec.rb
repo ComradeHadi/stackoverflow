49dbd68e79c0fd149b4ab38283e6746bc933aa11
Анатолий Удальцов <cmdx@tender.pro>
2015-03-28 23:14:10 +0300

    fix

--- spec/features/files/add_files_to_questions_spec.rb
+++ spec/features/files/add_files_to_questions_spec.rb
@@ -19,7 +19,7 @@ feature 'Add files to question', %q{
     attach_file 'File', "#{Rails.root}/spec/features/helper.rb"
     click_on 'Create'
 
-    expect(page).to have_content I18n.t('question.created')    
+    expect(page).to have_content I18n.t('question.created')
     expect(page).to have_link "helper.rb", href: "/uploads/attachment/file/1/helper.rb"
   end
 end
