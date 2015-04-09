5b863595253f5b023e9c48ed747069e3e9b77799
Анатолий Удальцов <cmdx@tender.pro>
2015-04-09 21:58:08 +0300

    fix

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Vote, type: :model do
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :like }
   
-  it { should validate_inclusion_of(:like).in_array( %w(-1, 1) ) } 
+  it { should validate_inclusion_of(:like).in_array( [Question::LIKE, Question::DISLIKE] ) } 
 
   describe 'Vote for question' do
     let(:users)      { create_list(:user, 2) }
@@ -15,7 +15,7 @@ RSpec.describe Vote, type: :model do
     let(:other_user) { users.at(1) }
     let(:question)   { create(:question) }
 
-    it 'author can not vote' do
+    it 'author can not vote for his question' do
       expect( question.user_can_vote? question.user ).to be false
       expect{ question.liked_by       question.user }.to_not change( question.votes, :count )
       expect{ question.downvote_by    question.user }.to_not change( question.votes, :count )
@@ -42,14 +42,14 @@ RSpec.describe Vote, type: :model do
       expect{ question.downvote_by    user }.to_not change( question.votes, :count )
     end
 
-    it 'withdraw vote first to vote againg' do
+    it 'withdraw vote to be able to vote again' do
       question.liked_by user
       question.withdraw_vote_by user
       expect( question.user_can_vote? user ).to be true
       expect{ question.downvote_by user }.to change( question.votes, :count ).by(1)
     end
 
-    it 'rating' do
+    it 'question rating' do
       expect( question.rating ).to eq 0
 
       expect{ question.liked_by user }.to change( question, :rating ).by(1)
