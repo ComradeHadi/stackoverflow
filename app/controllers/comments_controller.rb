2ef16d5b57b5907ce24ac31bff146d0f527a3267
Анатолий Удальцов <cmdx@tender.pro>
2015-04-25 23:06:29 +0300

    refactor comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_commentable_resource, only: :create
+  before_action :load_commentable, only: :create
   before_action :load_comment, only: :destroy
 
   def create
@@ -13,10 +13,8 @@ class CommentsController < ApplicationController
 
   private
 
-  def load_commentable_resource
-    commentable_name = params[:commentable]
-    commentable_id = "#{commentable_name}_id"
-    @commentable = commentable_name.classify.constantize.find(params[commentable_id])
+  def load_commentable
+    @commentable = commentable_klass.find params[commentable_id]
   end
 
   def load_comment
@@ -26,5 +24,17 @@ class CommentsController < ApplicationController
   def comment_params
     strong_params = params.require(:comment).permit(:body, :commentable)
     strong_params.merge( user_id: current_user.id ) if user_signed_in?
+  end
+
+  def commentable_name
+    params[:commentable]
+  end
+
+  def commentable_id
+    "#{commentable_name}_id"
+  end
+
+  def commentable_klass
+    commentable_name.classify.constantize
   end
 end
