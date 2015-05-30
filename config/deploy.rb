fa2c9375fa924b169948ec2a5117eb7e726c1ff1
Анатолий Удальцов <cmdx@tender.pro>
2015-05-30 23:22:50 +0300

    task private_pub:start, stop, restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,8 @@ set :deploy_user, 'deploy'
 set :linked_files, fetch(:linked_files, []).push(
   'config/database.yml',
   'config/secrets.yml',
-  'config/private_pub.yml'
+  'config/private_pub.yml',
+  'config/private_pub_thin.yml'
 )
 
 set :linked_dirs, fetch(:linked_dirs, []).push(
@@ -39,3 +40,44 @@ namespace :deploy do
     end
   end
 end
+
+namespace :private_pub do
+  set :private_pub_pid, -> { "#{current_path}/tmp/pids/private_pub.pid" }
+
+  desc 'start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:stage) do
+          execute :bundle, \
+            "exec thin -C config/private_pub_thin.yml -P #{fetch(:private_pub_pid)} start"
+        end
+      end
+    end
+  end
+
+  desc 'stop private_pub server'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:stage) do
+          execute "if [ -f #{fetch(:private_pub_pid)} ] && [ -e /proc/$(cat #{fetch(:private_pub_pid)}) ]; then kill -9 `cat #{fetch(:private_pub_pid)}`; fi"
+        end
+      end
+    end
+  end
+
+  desc 'restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:stage) do
+          invoke 'private_pub:stop'
+          invoke 'private_pub:start'
+        end
+      end
+    end
+  end
+end
+
+after 'deploy:restart', 'private_pub:restart'
